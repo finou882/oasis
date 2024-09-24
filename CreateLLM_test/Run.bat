ec29f2ddacdbb9feed338dd08c38370c50243967
FuMI(finou882) <146727978+finou882@users.noreply.github.com>
2024-09-25 00:06:51 +0900

    new

--- CreateLLM_test/Run.bat
+++ CreateLLM_test/Run.bat
@@ -1,11 +1,4 @@
-Rem/||(
-   Japaneese
-   5行目のダブルクオーテーションで囲われているパスをエンジンのrun.exeに変更してください。
-   English
-   Change the path enclosed in double quotes on the 5th line to the engine run.exe.
-   ^)
-  )
-start "C:\Users\finou\AppData\Local\Programs\VOICEVOX\vv-engine\run.exe"
+start "" "C:\Users\finou\AppData\Local\Programs\VOICEVOX\vv-engine\run.exe"
 python3 -m pip install --upgrade pip
 python3 -m pip install git+https://github.com/huggingface/accelerate
 python3 -m pip install datasets
